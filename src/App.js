5a87e34259a80adeccf1eac99168809cf09f131d
rodriguezal3x <alexjosuerm1995@gmail.com>
2022-09-02 09:28:12 -0600

    proyeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+import { useState } from "react";
 import { Formik, Form, Field } from "formik";
+import './header.css'
+import './content.css'
+import './article.css'
 
 
 const App =()=> {
-
+  const [photos, setPhotos] = useState([]);
+  const open = url => window.open(url)
+  console.log(photos)
   return (
     <div>
-      <headers>
+      <header>
         <Formik
           initialValues={{ search:'' }}
           onSubmit={ async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers:{
+                'Authorization' : 'Client-ID BKWCbinBJP7vAlSYypE9s7saKV-yqu-YpPf-Bv9UJwU'
+              }
+            })
+            const data = await response.json()
             // llamar a la api de unsplash
-            console.log(values)
+            setPhotos(data.results)
           }}
         >
           <Form>
             <Field name='search'/>
           </Form>
         </Formik>
-      </headers>``
+      </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo =>
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={ photo.urls.regular } />
+              <p>{[photo.description, photo.alt_description].join('-')}</p>
+            </article>)}
+        </div>
+      </div>
     </div> 
   );
 }
